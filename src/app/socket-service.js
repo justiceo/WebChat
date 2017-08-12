ccffb971990ec3cb48b67e5b4594e305cadd05b7
Justice Ogbonna <ebesco@yahoo.com>
2017-08-12 18:30:19 -0400

    attempting to add jwt

--- src/app/socket-service.js
+++ src/app/socket-service.js
@@ -1,6 +1,17 @@
 export default class SocketService {
     constructor() {
         this.io = io.connect('http://localhost:4000');
+        this.io.on('connection', () => {
+            this.io
+                .emit('authenticate', { token: "jwt" }) //send the jwt
+                .on('authenticated', (data) => {
+                    conosle.log("authenticated", data)
+                })
+                .on('unauthorized', (msg) => {
+                    console.log("unauthorized: " + JSON.stringify(msg.data));
+                    throw new Error(msg.data.type);
+                })
+        });
         console.log("initing socket")
     }
 }
--- src/server/server.js
+++ src/server/server.js
@@ -3,26 +3,25 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 var port = 4000;
-
+var socketioJwt = require('socketio-jwt');
+console.log("jwt: ", socketioJwt);
 // return any file in the build dir except server.js
-app.use('/', function(req, res, next) {
-    if(req.originalUrl === '/server.js') {
+app.use('/', function (req, res, next) {
+    if (req.originalUrl === '/server.js') {
         return res.status(403).end('403 Forbidden');
     }
     return res.sendFile(__dirname + req.originalUrl);
 });
 
-io.on('connection', function(client) {
-	console.log('Client connected...');
 
-	client.on('join', function(data) {
-		console.log(data);
-	});
-
-	client.on('messages', function(data){
-		client.emit('thread', data);
-		client.broadcast.emit('thread', data);
-	});
+io.on('connection', socketioJwt.authorize({
+    secret: 'your secret or public key',
+    timeout: 15000 // 15 seconds to send the authentication message
+})).on('authenticated', function (socket) {
+    //this socket is authenticated, we are good to handle more events from it.
+    console.log('hello! ' + socket.decoded_token.name);
+}).on('unauthenticated', (err) => {
+    console.log("not authenticated")
 });
 
 server.listen(port);
