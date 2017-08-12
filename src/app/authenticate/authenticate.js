842e2da78328852a7d9da8b405832e7461ed3549
Justice Ogbonna <ebesco@yahoo.com>
2017-08-12 19:37:02 -0400

    refresh qr code at interval

--- src/app/authenticate/authenticate.js
+++ src/app/authenticate/authenticate.js
@@ -1,10 +1,27 @@
 export class AuthCtrl {
-    constructor() {  
+    constructor($timeout) {
+        this.$timeout = $timeout;
+        this.updateQRCode(); 
     }
-    $onInit() {
-        window.QRCode.toDataURL('sample text dfadf fd adfdf af d fadf adf a daf afd', (err, url) => {
+
+    updateQRCode() {
+        // refresh the code every 8 secs
+        if(!this.authed)            
+            this.$timeout(() => {return this.updateQRCode()}, 8000)
+        // request new id from server and update code
+        window.QRCode.toDataURL(this.makeid(), (err, url) => {
             this.imgUrl = url;
         });
+    }
+
+    makeid() {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for (var i = 0; i < 100; i++)
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+        return text;
     }
 }
 
