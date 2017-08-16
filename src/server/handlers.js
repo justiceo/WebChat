764f5efaa6c077007591221737b455c4e39c50b7
Justice Ogbonna <ebesco@yahoo.com>
2017-08-16 00:04:40 -0400

    refactoring server.js

--- src/server/handlers.js
+++ src/server/handlers.js
@@ -1,6 +1,13 @@
 export default class Handlers {
-    constructor() {
+    constructor(clientManager) {
         this.TAG = "Handlers: ";
+        this.clientManager = clientManager;
         console.log(this.TAG, "initializing...")
+    }
+
+    garnish(io) {
+        io.on('connection', (socket) => {
+            console.log("connected");
+        })
     }
 }
