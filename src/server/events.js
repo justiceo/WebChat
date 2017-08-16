90ed7a1ac2e1671fd6840bf654d76eab22b71d7e
Justice Ogbonna <ebesco@yahoo.com>
2017-08-16 19:36:13 -0400

    add other default socket events

--- src/server/events.js
+++ src/server/events.js
@@ -1,5 +1,12 @@
 module.exports = {
+    // socket io events
+    CONNECT: "connect",
     CONNECTION: "connection",
+    DISCONNECT: "disconnect",       // Fired upon disconnection.
+    DISCONNECTING: "disconnecting", // Fired when the client is going to be disconnected (but hasn't left its rooms yet).
+    ERROR: "error",                 // Fired when an error occurs.
+
+    // custom events
     TOKEN_REQUEST: "tokenRequest",  // received from clients to obtain token
     TOKEN_REFRESH: "tokenRefresh",  // received from clients (with old tokens) for new ones
     REFRESH_FAIL: "refreshFail",    // emitted by server when previous token couldn't be validated
