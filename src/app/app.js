e6f61f7db0f0616a9c1c1f80e251e5bd612a5d44
Justice Ogbonna <ebesco@yahoo.com>
2017-08-16 10:53:35 -0400

    added error display and other stylings

--- src/app/app.js
+++ src/app/app.js
@@ -1,15 +1,17 @@
 
 export class AppCtrl {
     constructor() {
-
+        this.state = "notAuthed";
     }
 }
 
 let App = {
     template: `
-    <authenticate auth-state="$ctrl.state" ng-hide="$ctrl.state == 'authed'"></authenticate>
-    <conversations ng-show="$ctrl.state == 'authed'"></conversations>
-    <p ng-show="$ctrl.state == 'invalidClient'"></p>
+    <div class="wrapper" ng-switch="$ctrl.state">
+        <authenticate auth-state="$ctrl.state" ng-switch-when="notAuthed"></authenticate>
+        <conversations ng-switch-when="authed"></conversations>
+        <p ng-switch-default></p>
+    </div>
     `,
     controller: AppCtrl
 }
