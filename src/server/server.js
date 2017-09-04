ec8cd1de269f6e3179dbc77e1fbeff4fcef253c4
Justice Ogbonna <ebesco@yahoo.com>
2017-09-03 23:33:06 -0400

    added redis-server to node

--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,41 @@
 require('dotenv').config();
-let express     = require('express');
-let app         = express();
-let server      = require('http').createServer(app);
-let io          = require('socket.io')(server);
-let port        = process.env.PORT;
-let host        = process.env.HOST;
+let express = require('express');
+let app = express();
+let server = require('http').createServer(app);
+let io = require('socket.io')(server);
+let port = process.env.PORT;
+let host = process.env.HOST;
+let dbClient = require('redis').createClient();
+let RedisServer = require('redis-server');
+let dbServer = new RedisServer(6379);
 
-let ClientManager   = require('./client-manager');
-let Handlers        = require('./handlers');
+dbServer.open(err => {
+    if (err === null) {
+        dbClient.on('error', function (err) {
+            console.log('Error ' + err)
+        })
+
+        dbClient.set('string key', 'string val', dbClient.print)
+        dbClient.hset('hash key', 'hashtest 1', 'some value', dbClient.print)
+        dbClient.hset(['hash key', 'hashtest 2', 'some other value'], dbClient.print)
+
+        dbClient.hkeys('hash key', function (err, replies) {
+            console.log(replies.length + ' replies:')
+
+            replies.forEach(function (reply, i) {
+                console.log('    ' + i + ': ' + reply)
+            })
+
+            dbClient.quit()
+        });
+    }
+})
+
+
+
+
+let ClientManager = require('./client-manager');
+let Handlers = require('./handlers');
 //console.log(clientManagers, handlers);
 
 // return any file in the build dir except server.js
