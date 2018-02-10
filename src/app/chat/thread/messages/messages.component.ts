ca41c538c140d40e443de42c096e3eb0466dca91
Justice Ogbonna <justiceo@google.com>
2018-02-09 21:30:04 -0500

    started down on messages component

--- src/app/chat/thread/messages/messages.component.ts
+++ src/app/chat/thread/messages/messages.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MessagesComponent implements OnInit {
 
+  messages = [1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7];
   constructor() { }
 
   ngOnInit() {
