b2433adedca6ebf212723b2ba98bf4ff5fad2e10
Justice Ogbonna <ebesco@yahoo.com>
2018-02-25 14:58:49 -0500

    hide bootrap arrow on dropdown. Title case names

--- src/app/chat/threads/threads.component.ts
+++ src/app/chat/threads/threads.component.ts
@@ -11,16 +11,20 @@ export class ThreadsComponent implements OnInit {
   threads = [];
   constructor(private dataService: DataService) {
     this.dataService.getRandomUsers().subscribe(data => {
-      for(let user of data['results']) {
+      for (let user of data['results']) {
         this.threads.push({
-            'name': user['name']['first'] + ' ' + user['name']['last'],
-            'avatarUrl': user['picture']['large'],
-            'body': user['cell'],
+          'name': this.toTitleCase(user['name']['first']) + ' ' + this.toTitleCase(user['name']['last']),
+          'avatarUrl': user['picture']['large'],
+          'body': user['cell'],
         });
       }
     });
-   }
+  }
 
   ngOnInit() {
+  }
+
+  toTitleCase(str) {
+    return str.replace(/\w\S*/g, function (txt) { return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase(); });
   }
 }
