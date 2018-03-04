e37bbeec33d10b78e28fdd912a4a6d8c4f7b3d4b
Justice Ogbonna <justiceo@justiceo-macbookpro1.roam.corp.google.com>
2018-03-04 14:28:26 -0500

    make app.components.scss a global css file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `<app-chat></app-chat>`,
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   title = 'app';
