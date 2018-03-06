ad482ceab28539252d29793ecdb30b6def39848a
Justice Ogbonna <ebesco@yahoo.com>
2018-03-05 21:21:35 -0500

    add custom date pipe for day separator

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -7,13 +7,14 @@ import { ThreadsComponent } from './threads/threads.component';
 import { ChatComponent } from './chat.component';
 import { ProfileSettingsComponent } from './profile-settings/profile-settings.component';
 import { ContactInfoComponent } from './contact-info/contact-info.component';
+import { TimeSince } from '../timesince.pipe';
 
 @NgModule({
   imports: [
     CommonModule,
     FlexLayoutModule,
   ],
-  declarations: [ThreadComponent, ThreadsComponent, ChatComponent, ProfileSettingsComponent, ContactInfoComponent],
-  exports: [ ThreadComponent, ThreadsComponent, ChatComponent, ProfileSettingsComponent, ContactInfoComponent],
+  declarations: [ThreadComponent, ThreadsComponent, ChatComponent, ProfileSettingsComponent, ContactInfoComponent, TimeSince],
+  exports: [ ThreadComponent, ThreadsComponent, ChatComponent, ProfileSettingsComponent, ContactInfoComponent, TimeSince],
 })
 export class ChatModule { }
