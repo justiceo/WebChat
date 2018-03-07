32d0b4e9abcbc31d4036495356444b43d00787b6
Justice Ogbonna <ebesco@yahoo.com>
2018-03-07 11:11:15 -0500

    icons engineering. profile settings ui

--- src/app/chat/profile-settings/profile-settings.component.ts
+++ src/app/chat/profile-settings/profile-settings.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-profile-settings',
   templateUrl: './profile-settings.component.html',
-  styleUrls: ['./profile-settings.component.css']
+  styleUrls: ['./profile-settings.component.scss']
 })
 export class ProfileSettingsComponent implements OnInit {
 
