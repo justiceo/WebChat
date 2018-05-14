5732c520da6592d263a722ddd8ec21dda2b7d2da
Justice Ogbonna <ebesco@yahoo.com>
2018-05-13 22:51:09 -0400

    prep threads-list to remove angular flex

--- src/app/chat/threads-list/threads-list.component.ts
+++ src/app/chat/threads-list/threads-list.component.ts
@@ -1,28 +1,27 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { Observable } from 'rxjs';
-import { bufferCount } from 'rxjs/operators';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { Observable } from "rxjs";
+import { bufferCount } from "rxjs/operators";
 
-import { Thread } from '../../model/thread';
-import { DataService } from '../../services/data.service';
+import { Thread } from "../../model/thread";
+import { DataService } from "../../services/data.service";
 
 @Component({
-  selector: 'wc-threads-list',
-  templateUrl: './threads-list.component.html',
-  styleUrls: ['./threads-list.component.scss']
+  selector: "wc-threads-list",
+  templateUrl: "./threads-list.component.html",
+  styleUrls: ["./threads-list.component.scss"]
 })
 export class ThreadsListComponent implements OnInit {
   threads: Thread[];
   @Input() current: Thread;
   @Output() threadChange = new EventEmitter<Thread>();
 
-  avatarUrl = 'https://randomuser.me/api/portraits/men/43.jpg';
-  name = 'John Doe';
-  notification = { type: 'EnableNotifications'};
+  avatarUrl = "https://randomuser.me/api/portraits/men/43.jpg";
+  name = "John Doe";
+  notification = { type: "EnableNotifications" };
 
   constructor(private dataService: DataService) {
     this.threads = dataService.getThreads();
-    // this.current = this.threads[0]; // uncomment to use first thread as default
-    this.current = new Thread();
+    this.current = this.threads[0];
   }
 
   changeThread(t: Thread) {
@@ -31,6 +30,6 @@ export class ThreadsListComponent implements OnInit {
   }
 
   ngOnInit() {
-    // this.threadChange.emit(this.current);
+    this.threadChange.emit(this.current);
   }
 }
