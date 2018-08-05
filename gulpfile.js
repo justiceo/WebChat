cc1abde892c2ff013f5d4a52904a27b0ac2c546b
Justice Ogbonna <ebesco@yahoo.com>
2018-08-05 04:38:27 -0400

    add incremental compilation for server files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require("gulp");
 var ts = require("gulp-typescript");
 
-var tsProject = ts.createProject("tsconfig.json", {});
-gulp.task("default", function() {
+var tsProject = ts.createProject("tsconfig.json", { declaration: true });
+var source = ["src/common/*.ts", "src/server/*.ts"];
+
+gulp.task("build", function() {
   return gulp
-    .src(["src/common/*.ts", "src/server/*.ts"])
+    .src(source)
     .pipe(tsProject())
-    .pipe(gulp.dest("./dist/"));
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("default", ["build"], function() {
+  gulp.watch(source, ["build"]);
 });
